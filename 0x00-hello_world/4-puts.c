e6af3cde52d789e61e202a52de8c30817e855599
root <dafelaurele@gmail.com>
2022-03-10 16:10:03 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
 /**
  *  * main - Entry point
  *   *
+ *   Description: prints "Programming is like building a multilingual puzzle"
+ *
  *    * Return: Always 0 (Success)
  *     */
 int main(void)
@@ -10,4 +12,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");	
 	    return (0);
 }
-
