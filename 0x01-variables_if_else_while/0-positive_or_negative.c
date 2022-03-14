51b34839f070ec88d9f70ea88d1aa7642f85eaaf
root <dafelaurele@gmail.com>
2022-03-14 10:43:16 -0700

    positive negative numb

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,31 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-/**
- * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main- assigns a new number to int n and checks if it is positive, negative
+ * and zero. It executes and prints
  * Return: 0
  */
 int main(void)
 {
-	int n;
+		int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
-
-	return (0);
-}}
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/*
+					 *int n = 9;
+					 *if n > 0;
+					 *printf("%i",is positive/n);
+					 *
+					 *int n = 8;
+					 *if n == 0;
+					 *printf("%i",is zero/n);
+					 *
+					 *int n = 7;
+					 *if n < 0;
+					 *printf("%i",is negative/n)
+					 *
+					 */
+					return (0);
+}
